f9b2edd97f0523e1c5d6239f3447e0aee11b32aa
Farshid Karimi <farshidk49@gmail.com>
2022-04-04 17:03:17 +0430

    filter added

--- src/components/Homes.js
+++ src/components/Homes.js
@@ -9,7 +9,7 @@ const Homes = ({ homes }) => {
   return (
     <div className="d-flex flex-wrap justify-content-between">
       {homes ? (
-        homes.filter((home)=> home.city !== filteredCities).map((home) => (
+        homes.filter(filteredCities!==""?(home)=> home.city === filteredCities:(home)=> home).map((home) => (
           <div className="stay_items" key={home.title}>
             <img src={home.photo} alt="Home Photo"></img>
             <div className="d-flex">
